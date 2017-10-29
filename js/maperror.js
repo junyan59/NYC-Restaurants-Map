9a2ec5ee1b8a4c0184d4a73198e30194a9397ffd
Jun Yan <stephanieyan59@gmail.com>
2017-10-28 22:21:12 -0400

    fix: create map error js file

---
+++ js/maperror.js
@@ -0,0 +1,5 @@
+// Use vanilla JS only in case jQuery doesn't load
+var mapError = function() {
+    document.getElementById('map-error').style.display = 'block';
+    document.getElementById('map-error').innerHTML = 'Sorry, something went wrong. Please try again later.';
+};
